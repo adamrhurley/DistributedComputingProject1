a4728331166aec5a93acbc6b72bfcbc013cabbf9
Adam Hurley <adam.hurley@students.ittralee.ie>
2021-03-04 18:38:00 +0000

    Created Basic JFrame

---
+++ src/Login.java
@@ -0,0 +1,9 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Login extends JFrame {
+    public static void login() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.BorderLayout;
+
+    public class Main extends JFrame {
+        static JFrame frame;
+
+        static JButton buttonLogin;
+
+        static JLabel usernameLabel,passwordLabel;
+
+        static JTextField usernameText,passwordText;
+
+        public static void main(String[] args) {
+
+            frame = new JFrame("Login");
+
+            // create a label to display text
+            usernameLabel = new JLabel("Username");
+            passwordLabel = new JLabel("Password");
+            // create a new buttons
+            buttonLogin = new JButton("Login");
+
+            usernameText = new JTextField();
+            usernameText.setPreferredSize(new Dimension(200,30) );
+            usernameText.setMaximumSize(new Dimension(200,30) );
+
+            passwordText = new JTextField();
+            passwordText.setPreferredSize(new Dimension(200,30) );
+            passwordText.setMaximumSize(new Dimension(200,30) );
+
+            // create a panel to add buttons
+            JPanel panel = new JPanel();
+
+
+
+            //add buttons and textfield to panel
+            panel.add(usernameLabel);
+            panel.add(usernameText);
+            panel.add(passwordLabel);
+            panel.add(passwordText);
+            panel.add(buttonLogin,BorderLayout.SOUTH);
+
+            buttonLogin.addActionListener(e -> Login.login());
+
+            // set the size of frame
+            frame.setSize(1000, 600);
+            frame.add(panel);
+            frame.setVisible(true);
+        }
+    }
